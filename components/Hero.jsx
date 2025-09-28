bafeff6b3757a7f77f6e5c08ff9c6d342469d472
SIam <siamazizulhakim95@gmail.com>
2025-09-28 20:52:36 +0600

    feat: Update tsconfig and Hero component
    Refactors tsconfig for modern TypeScript settings and adjusts Hero component's heading font weight for better visual hierarchy.

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
       {/* Content */}
       <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-[75vh] lg:items-center lg:px-8">
         <div className="max-w-xl text-center sm:text-left">
-          <h1 className="text-4xl font-extralight sm:text-6xl tracking-wider uppercase">
+          <h1 className="text-4xl font-bold sm:text-6xl tracking-wider uppercase">
             SHOPHORIA
           </h1>
 
